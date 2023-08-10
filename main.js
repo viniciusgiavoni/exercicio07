bbdaebf4dcbc21964d99d00d44ccf15daa09b0bf
Vinicius <vini.giavoni@gmail.com>
2023-08-09 23:07:04 -0300

    exercicio mod07

---
+++ main.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('form-valida');
+const valorOne = document.getElementById('valor-one');
+const valorTwo = document.getElementById('valor-two');
+
+function validar(valorOne, valorTwo) {
+    return valorTwo > valorOne;
+}
+
+form.addEventListener('submit', function(e) {
+    let formValida
+    e.preventDefault();
+
+    const mensagemSucesso = `Você enviou os valores: ${valorOne} e ${valorTwo} com sucesso`;
+    const mensagemErro = `O valor do segundo depósito deve ser maior que o primeiro!`;
+
+    formValida = validar(valorOne, valorTwo)
+    if (formValida) {
+        const containerMensagemSucesso = document.querySelector('.success-mesage');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+
+        valorOne.value =' ';
+        valorTwo.value =' ';
+
+    } else {
+        const containerMensagemErro = document.querySelector('.success-mesage');
+        containerMensagemErro.innerHTML = mensagemErro;
+        containerMensagemErro.style.display = 'block';
+    }
+   
+})
